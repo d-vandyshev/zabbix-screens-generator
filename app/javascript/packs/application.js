af9c129854bc1f2525c2492874553d8155b2eadf
Dmitry Vandyshev <d.vandyshev@ya.ru>
2018-03-30 16:35:55 +0500

    Added rails-ujs and turbolinks to js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,12 @@
 
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
+import Turbolinks from "turbolinks";
+import Rails from "rails-ujs";
 
 const application = Application.start()
 const context = require.context("./controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+Rails.start();
+Turbolinks.start();
