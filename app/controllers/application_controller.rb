e4c44dd45fb80102637963d0d2bcae0058070007
Dmitry Vandyshev <d.vandyshev@ya.ru>
2018-03-30 16:40:00 +0500

    added def generator_post in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include Turbolinks::Redirection
 
   include SessionsHelper
 
@@ -10,5 +11,29 @@ class ApplicationController < ActionController::Base
 
     zabbix = Rails.cache.read(session[:uuid])
     @hostgroups_array = zabbix.hostgroups
+  end
+
+  def generator_post
+    unless logged_in?
+      redirect_to(root_path) and return
+    end
+
+    zabbix = Rails.cache.read(session[:uuid])
+    @hostgroups_array = zabbix.hostgroups
+    @hosts = zabbix.hosts_by_hostgroup_id(hostgroup_params.fetch('id'))
+    puts @hosts.inspect
+
+    # puts hostgroup_param['id']
+    # @hosts = [
+    #     {name: 'firstname', ip: hostgroup_params},
+    #     {name: 'secondname', ip: '1.1.2.2'}
+    # ]
+    render 'generator'
+  end
+
+  private
+
+  def hostgroup_params
+    params.require(:hostgroup).permit(:id)
   end
 end
