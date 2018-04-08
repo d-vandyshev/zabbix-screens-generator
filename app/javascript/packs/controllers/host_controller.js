941956e7d23416bfd18b44d58d8364ed765b7279
Dmitry Vandyshev <d.vandyshev@ya.ru>
2018-04-08 12:07:55 +0500

    Select all hosts when click for "Check all"

--- app/javascript/packs/controllers/host_controller.js
+++ app/javascript/packs/controllers/host_controller.js
@@ -1,11 +1,24 @@
-import { Controller } from 'stimulus'
+import {Controller} from 'stimulus'
 
 export default class extends Controller {
-    static targets = ["hostgroup_id", "submit"]
+    static targets = ["hostgroup_id", "submit", "check", "checkall"]
 
     changeHostgroup() {
-        console.log(`Hello, ${this.hostgroup_id}!`);
         this.submitTarget.click();
+    }
+
+    toggleAll() {
+        this.checkTargets.forEach((c) => {
+            if (this.checkall) {
+                c.checked = true
+            } else {
+                c.checked = false
+            }
+        })
+    }
+
+    get checkall() {
+        return this.checkallTarget.checked
     }
 
     get hostgroup_id() {
