ba3f85300ebc373e8486270e9953a3fe7c7c7e10
Dmitry Vandyshev <d.vandyshev@ya.ru>
2018-05-10 20:54:45 +0500

    Set underline label of the replace checkbox if it's checked

--- app/javascript/packs/controllers/host_controller.js
+++ app/javascript/packs/controllers/host_controller.js
@@ -1,7 +1,8 @@
 import {Controller} from 'stimulus'
 
 export default class extends Controller {
-    static targets = ["hostgroup_id", "submit", "check", "checkall", "modal_no_hosts_selected"]
+    static targets = ["hostgroup_id", "submit", "check", "checkall", "modal_no_hosts_selected",
+        "replace_label", "replace_checkbox"]
 
     changeHostgroup() {
         this.submitTarget.click();
@@ -36,10 +37,20 @@ export default class extends Controller {
 
     switchHighlightRow(checkbox) {
         let tr = checkbox.parentNode.parentNode
-        if(checkbox.checked) {
+        if (checkbox.checked) {
             tr.classList.add("table-info")
         } else {
             tr.classList.remove("table-info")
+        }
+    }
+
+    underlineCheckboxLabel() {
+        let rl = this.replace_labelTarget
+        let rc = this.replace_checkboxTarget
+        if (rc.checked) {
+            rl.classList.add("underline")
+        } else {
+            rl.classList.remove("underline")
         }
     }
 
