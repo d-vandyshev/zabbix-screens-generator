31114ea248cc779ddf9e8916d809a7ea67d227b5
Dmitry Vandyshev <d.vandyshev@ya.ru>
2018-05-23 23:52:42 +0500

    Added tests for special characters in Credentials attributes

--- test/models/credentials_test.rb
+++ test/models/credentials_test.rb
@@ -6,4 +6,18 @@ class CredentialsTest < ActiveSupport::TestCase
     assert_not creds.valid?
     assert_equal [:server, :username, :password], creds.errors.keys
   end
+
+  test "server and username should be valid without special characters" do
+    %w[1.1.1.1 zabbix первый-admin].each do |str|
+      creds = Credentials.new(server: str, username: str, password: 'passw')
+      assert creds.valid?
+    end
+  end
+
+  test "server and username should be invalid with special characters" do
+    %w[http://1.1.1.1 zab*bix adm&%$in].each do |str|
+      creds = Credentials.new(server: str, username: str, password: 'passw')
+      assert_not creds.valid?
+    end
+  end
 end
