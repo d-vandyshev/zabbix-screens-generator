76abacc8e752c4d3a04b72a6151405e8013e3e47
Dmitry Vandyshev <d.vandyshev@ya.ru>
2018-06-25 23:26:26 +0500

    Improved tests in SessionsControllerTest.rb

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -9,21 +9,30 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_instance_of Credentials, @controller.instance_variable_get(:@credentials)
   end
 
-  test 'new should show form' do
+  test 'new should show form in not logged_in?' do
     SessionsController.stub_any_instance(:logged_in?, false) do
       get root_url
     end
     assert_response :success
+
+    # test logged_in? from SessionsHelper module
+    assert_not @controller.logged_in?
+    session[:uuid] = 'uuid2335'
+    Rails.cache.write(session[:uuid], 'uuid2335')
+    assert @controller.logged_in?
   end
 
   test 'create should set_session and redirect if valid?' do
     ZabbixService.stub(:new, :zabbixapi) do
-      SessionsController.stub_any_instance(:set_session, true) do
-        post '/', params: {credentials: {server: 'server', username: 'username', password: 'password'}}
-      end
+      post '/', params: {credentials: {server: 'server', username: 'username', password: 'password'}}
     end
     assert_instance_of Credentials, @controller.instance_variable_get(:@credentials)
     assert_redirected_to screens_new_path
+
+    # test set_session from SessionsHelper module
+    assert_instance_of String, session[:uuid]
+    assert_equal 'username', session[:username]
+    assert_equal :zabbixapi, Rails.cache.read(session[:uuid])
   end
 
   test 'create should render new and flash if not valid?' do
@@ -55,10 +64,13 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'destroy should destroy_session flash and redirect' do
-    SessionsController.stub_any_instance(:destroy_session, :true) do
-      delete logout_url
-    end
+    delete logout_url
     assert_not flash.empty?
     assert_redirected_to root_url
+
+    # test destroy_session from SessionsHelper module
+    assert_nil Rails.cache.read(session[:uuid])
+    assert_nil session[:uuid]
+    assert_nil session[:username]
   end
 end
