8175a0a4e0d7dff2bf219ef03d378daa29228377
Dmitry Vandyshev <d.vandyshev@ya.ru>
2018-07-14 20:31:51 +0500

    rails g system_test creating_screens

---
+++ test/system/creating_screens_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class CreatingScreensTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit creating_screens_url
+  #
+  #   assert_selector "h1", text: "CreatingScreens"
+  # end
+end
