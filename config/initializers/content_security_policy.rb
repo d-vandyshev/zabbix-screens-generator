d36683aa42a169623d5b156c7c60537e19b33be3
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-01-10 05:52:49 +0500

    fix(policy): Fix error: undefined local variable or method `policy'
    'rails server' command
    fail:
    /opt/zabbix-screens-generator/config/initializers/content_security_policy.rb:1:in
    `<top (required)>': undefined local variable or method `policy' for
    main:Object (NameError)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,1 +1,3 @@
-policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+Rails.application.config.content_security_policy do |policy|
+  policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+end
