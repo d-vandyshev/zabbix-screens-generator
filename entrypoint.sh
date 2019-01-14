699c88c1c62423cd563e314978925882a890bba5
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-01-14 10:12:01 +0500

    build(docker): Move ENV vars from docker-compose.yml to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 rm -f /app/tmp/pids/server.pid
 export SECRET_KEY_BASE=$(rails secret)
-
+export RAILS_ENV=production
+export RAILS_SERVE_STATIC_FILES=yes
+export RAILS_LOG_TO_STDOUT=yes
 bundle exec rails server
