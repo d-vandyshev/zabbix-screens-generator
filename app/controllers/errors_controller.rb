d7e6b970cc2c51bd09e77481627ca553af8e6b15
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-01-22 00:24:10 +0500

    feat(errors): Change en to I18n messages about errors

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
-    flash.now[:danger] = 'Page not found (400)'
+    flash.now[:danger] = I18n.t 'errors.page_not_found'
     render status: 404
   end
 
   def internal_server_error
-    flash.now[:danger] = 'An Internal Server Error occured (500)'
+    flash.now[:danger] = I18n.t 'errors.internal_server_error'
     render status: 500
   end
 end
