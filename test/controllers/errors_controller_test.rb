58ab5886cfc489a76597491f4b7dc1c3c5fee200
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-01-22 00:25:12 +0500

    test(errors): Test errors actions 404 and 500

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,14 +1,28 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get not_found" do
-    get errors_not_found_url
-    assert_response :success
+  test "incident page_not_found should call their method" do
+    get '/unknown-url'
+    assert_response :missing
+
+    get '/404'
+    assert_response :missing
   end
 
-  test "should get internal_server_error" do
-    get errors_internal_server_error_url
-    assert_response :success
-  end
+  test "incident internal_server_error should call their method" do
+    class ::TestxController < ApplicationController
+      def testx
+        raise StandardError
+      end
+    end
+    Rails.application.routes.draw do
+      get 'test' => 'testx#testx'
+    end
+    get test_url
+    assert_response :error
+    Rails.application.reload_routes!
 
+    get '/500'
+    assert_response :error
+  end
 end
