fb3f29f8b9c6a97e6d0e3005d9a9159263f3de42
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-02-08 10:01:51 +0500

    refactor(controller): Replace http error codes with their Rails symbols

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
     flash.now[:danger] = I18n.t 'errors.page_not_found'
-    render status: 404
+    render status: :not_found
   end
 
   def internal_server_error
     flash.now[:danger] = I18n.t 'errors.internal_server_error'
-    render status: 500
+    render status: :internal_server_error
   end
 end
