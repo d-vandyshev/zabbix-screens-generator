1effdb0556dda8a619b079404e26965da9ed8ca4
Dmitry Vandyshev <d.vandyshev@ya.ru>
2019-03-18 05:45:21 +0500

    feat(webpacker): Set bootstrap to load

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
-const { environment } = require('@rails/webpacker')
+const {environment} = require('@rails/webpacker')
+
+// For Bootstrap modal on Step 2
+const webpack = require('webpack')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+}))
 
 module.exports = environment
